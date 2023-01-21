46487e3e4a94c03b8887a34f30010f2cf0f3f4fc
Guilherme de Oliveira <smo.guilherme93@gmail.com>
2023-01-21 14:24:32 -0300

    feat: added summary GET Route.

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -111,5 +111,27 @@ export async function appRoutes(app: FastifyInstance) {
     }
   });
 
-  app.get("/summary", async () => {});
+  app.get("/summary", async () => {
+    // so funciona em SQLite
+    const summary = await prisma.$queryRaw`
+      SELECT D.id, D.date, 
+        (
+          SELECT cast(count(*) as float)
+          FROM day_habits DH
+          WHERE DH.day_id = D.id
+
+        ) as completed,
+        (
+          SELECT cast(count(*) as float)
+          FROM habit_week_day HWD
+          JOIN habits H
+           ON H.id = HWD.habit_id
+          WHERE HWD.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
+            AND H.created_at <= D.date
+        ) as amount
+      FROM days D
+    `;
+
+    return summary;
+  });
 }
